2820ae6dad60957b9bcd01d186f51b36bf37c33f
touati soukaina <touatisoukaina1@gmail.com>
2026-02-15 21:20:26 +0100

    modif

--- scripts/run_full_pipeline.py
+++ scripts/run_full_pipeline.py
@@ -4,30 +4,24 @@ import logging
 from backend.app.core.logging_config import setup_logging
 from backend.app.data_preparation.pipelines.chunking_pipeline import ChunkingPipeline
 from backend.app.workers.indexing_worker import IndexingWorker
-
+from backend.app.data_preparation.pipelines.extraction_pipeline import run_extraction
+from scripts.backfill_indexing_jobs import main as backfill
 
 def main():
     setup_logging(app_name="app")
     log = logging.getLogger("scripts.run_full_pipeline")
 
-    # 1) PARSING (si tu veux tout faire en une commande)
-    # -----------------------------------------------
-    # Exemple (à adapter) :
-    # parser = WhoPdfParser()
-    # parser.process_file(file_paths=[...])  # ou ton pipeline d'extraction
+    # Parsing 
+    run_extraction()
+    
+    # Run chunking pipeline 
+    ChunkingPipeline().run()
 
-    # 2) CHUNKING
-    # -----------------------------------------------
-    # log.info("=== CHUNKING ===")
-    # ChunkingPipeline().run()
+    # BACKFILL 
 
-    # 3) BACKFILL (au cas où)
-    # -----------------------------------------------
-    # log.info("=== BACKFILL work_items (indexing) ===")
-    from scripts.backfill_indexing_jobs import main as backfill
     backfill(limit_chunks=5000)
 
-    # 4) INDEXING WORKER (embedding + upload Azure Search)
+    # INDEXING WORKER (embedding + upload Azure Search)
     # -----------------------------------------------
     log.info("=== INDEXING WORKER ===")
     worker = IndexingWorker(worker_id="local-worker")
