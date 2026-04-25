72e70129710545350609a99fa5b0f92d64bc1225
touati soukaina <touatisoukaina1@gmail.com>
2026-04-26 00:33:03 +0200

    Remove unused image parser and doctr dependency

--- backend/app/data_preparation/parsers/__init__.py
+++ backend/app/data_preparation/parsers/__init__.py
@@ -1,6 +1,5 @@
 from .base_parser import BaseParser
-from .image_parser import DoctrOCR
 from .pdf_parser import PdfParser
 from .txt_parser import TxtParser
 
-__all__ = ['BaseParser', 'DoctrOCR', 'PdfParser', 'TxtParser']
+__all__ = ['BaseParser', 'PdfParser', 'TxtParser']
